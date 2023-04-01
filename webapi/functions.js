7842f1df924aebdb5c9df003d1c5b38ec79b87bf
Sanket Salunke <salunkess2001@gmail.com>
2023-04-01 10:33:39 +0530

    Added tests
    Signed-off-by: Sanket Salunke <salunkess2001@gmail.com>

---
+++ webapi/functions.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const absSub = (a, b) => {
+  return Math.abs(a - b);
+};
+
+module.exports = {
+  sum: sum,
+  absSub: absSub,
+};
---
+++ webapi/tests/functions.test.js
@@ -0,0 +1,25 @@
+const functions = require("../functions");
+const request = require("supertest");
+const app = require("../index");
+
+it("Adds 1+3 equals to 4", () => {
+  expect(functions.sum(1, 3)).toBe(4);
+});
+
+it("Abs Subtract 3-5 equals to 2", () => {
+  expect(functions.absSub(3, 5)).toBe(2);
+});
+
+describe("Endpoint Tests", () => {
+  it("Test / endpoint", async () => {
+    const res = await request(app.app).get("/");
+    app.server.close();
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("Test /targets endpoint", async () => {
+    const res = await request(app.app).get("/targets");
+    app.server.close();
+    expect(res.statusCode).toBe(200);
+  });
+});
